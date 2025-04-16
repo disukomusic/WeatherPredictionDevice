55c5138e10ccf34c557548d3b1f78a05b526d914
Disuko <64217407+disukomusic@users.noreply.github.com>
2025-04-16 16:59:46 -0400

    Starts of getting json from Jeffery's server

--- ReadWeatherFromJson.py
+++ ReadWeatherFromJson.py
@@ -1,12 +1,20 @@
-﻿import json
+﻿import os
+import json
 import serial
+import requests
 
-arduino = serial.Serial(port='COM6',   baudrate=9600, timeout=.1)
+# Open serial comms with the Arduino
+# arduino = serial.Serial(port='COM6',   baudrate=9600, timeout=.1)
 
-filePath = 'sampleWeatherData.json'
+# HTTP Request to get weather data. Will be replaced with Jeffery's server instead
+response = requests.get("https://raw.githubusercontent.com/disukomusic/WeatherPredictionDevice/refs/heads/main/sampleWeatherData.json")
+data = response.text
+data = str(bytes(data, "utf-8")[3:])
+data = json.loads(data).decode("utf-8")
 
-with open(filePath, encoding="utf-8-sig") as json_file:
-    data = json.load(json_file)
+# local file
+# with open(filePath, encoding="utf-8-sig") as json_file:
+#     data = json.load(json_file)
 
 def getTemp():
     temp = data.get("Temp", "")
@@ -30,4 +38,4 @@ print(f"CO2 level is {co2}ppm")
 
 #thanks chatgpt
 message = f"{temp} {humid} {co2}\n"
-arduino.write(message.encode('utf-8'))
+# arduino.write(message.encode('utf-8'))
---
+++ TestRequest.py
@@ -0,0 +1,9 @@
+﻿import os
+import requests
+import json
+
+response = requests.get("http://192.168.0.119:80/latest")
+data = response.text
+data = json.loads(data).decode("utf-8-sig")
+
+print(data)
